c7a4b5eff8690c3475df7b80f52d01ee447149a9
Iain Lane <iain@orangesquash.org.uk>
2024-05-19 17:20:31 +0000

    build: Switch to Yarn 4
    This is faster and has a better resolution algorithm.

--- .devcontainer/onCreateCommand.sh
+++ .devcontainer/onCreateCommand.sh
@@ -2,8 +2,10 @@
 
 set -eu
 
+sudo chown node:node /home/node/.cache
+sudo chown node:node /home/node/.cache/node
 sudo chown node:node /home/node/.cache/yarn
 sudo chown node:node node_modules */*/node_modules
 
-yarn install --frozen-lockfile
+yarn install --immutable < /dev/null
 yarn serverless dynamodb install
